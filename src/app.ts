759cf815b3621dd99ebc2276616005ecdfa15b39
Tristan Tarpley <tristan@marketr.life>
2020-05-07 18:11:50 -0500

    swapped origin in get

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000
 const app = express()
 const test_var: number = 1
 
-app.get('/', (req, res) => res.send(req.get('host')))
+app.get('/', (req, res) => res.send(req.get('origin')))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
