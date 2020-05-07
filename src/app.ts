74acb269a8535c0c8c00cd051aacbceee58a493f
Tristan Tarpley <tristan@marketr.life>
2020-05-07 18:09:53 -0500

    testing host

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000
 const app = express()
 const test_var: number = 1
 
-app.get('/', (req, res) => res.send(`hello world! ${test_var}`))
+app.get('/', (req, res) => res.send(req.get('host')))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
