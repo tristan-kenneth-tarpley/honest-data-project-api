bbbfad1a7cc6cf487a2096c54bad44480ff9f031
Tristan Tarpley <tristan@marketr.life>
2020-05-07 17:19:50 -0500

    procfile

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+express()
+  .get('/', (req, res) => res.send('hello world'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
