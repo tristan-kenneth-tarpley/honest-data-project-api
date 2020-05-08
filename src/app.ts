45f08af9c41b02b9e423bd1b79aa8b2a5a993204
Tristan Tarpley <tristan@marketr.life>
2020-05-07 19:38:00 -0500

    finished build config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
-  
+import test from './import_test'  
+
 const express = require('express')
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-const test_var: number = 1
 
-app.get('/', (req, res) => res.send(req.get('host')))
+console.log('testing 123')
+test()
+var whitelist: Array<string> = [
+  'http://127.0.0.1:5000/', 'http://127.0.0.1:5000',
+  'http://localhost:3000/', 'http://localhost:3000',
+  'https://honestdata.world/', 'https://honestdata.world'
+]
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.get('/', cors(corsOptions), (req, res) => {
+    res.json({host: `${req.get('origin') ? req.get('origin') : req.get('host')}`})
+})
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
